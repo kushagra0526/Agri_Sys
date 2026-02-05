f994d9ceabda669e6ca95a09f21f32c640a904a4
Kushagra Pratap Singh <rajputkushagra26@gmail.com>
2026-02-06 02:03:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const { register, login, logout, resetpasswordutil } = require("./controllers/authController");
+const userRouter = require("./routers/userRouter");
+const seedRouter = require("./routers/seedRouter");
+const cookieParser = require("cookie-parser");
+const { verifyemail } = require("./controllers/userController");
+const eventsRouter = require("./routers/eventsRouter");
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());;
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.post("/api/auth/register",register);
+app.post("/api/auth/login",login);
+app.get("/api/auth/logout",logout);
+app.use("/api/users",userRouter);
+app.use("/api/seeds",seedRouter);
+app.get("/api/verify/:token", verifyemail);
+app.use("/api/events", eventsRouter);
+app.post("/api/resetpassword/:token",resetpasswordutil);
---
+++ db.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require("sequelize");
+
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT,
+  }
+);
+
+sequelize.authenticate().then(() => {
+  console.log("Database Connected");
+})
+  .catch((err) => {
+    console.log("Unable to connect to database",err);
+  });
+
+module.exports.sequelize = sequelize;
